7a178242e6a5f5302f3d25da94753d8313e4ab35
Batajus <ott-alexander@gmx.net>
2022-01-30 19:50:15 +0100

    test: added unit tests for the NavigationService

--- src/app/navigation.service.spec.ts
+++ src/app/navigation.service.spec.ts
@@ -1,5 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { debounceTime, first, skip } from 'rxjs/operators';
 
 import { NavigationService } from './navigation.service';
 
@@ -11,9 +13,34 @@ describe('NavigationService', () => {
       imports: [RouterTestingModule],
     });
     service = TestBed.inject(NavigationService);
+
+    // Increased default timeout due to duration of the mocked http request
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1500;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('get navigation items - empty list without routes', (done) => {
+    let supportCounter = 0;
+    service.getNavigationItems().subscribe((links) => {
+      expect(links).toEqual([]);
+      if (supportCounter === 1) {
+        done();
+      }
+      supportCounter++;
+    });
+  });
+
+  it('get navigation items - ', inject([Router], (router: Router) => {
+    router.config = [{ path: 'a', component: null }];
+
+    service
+      .getNavigationItems()
+      .pipe(first())
+      .subscribe((links) => {
+        expect(links).toEqual([{ title: 'A', route: 'a' }]);
+      });
+  }));
 });
